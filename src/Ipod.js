99efc9c0ceb8ef00b3e557806fb4c986dc20692a
sourabh <sourabhkumar0308@gmail.com>
2021-02-21 16:18:24 +0530

    added zingTouch library and created wheel UI

---
+++ src/Ipod.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Wheel from './Wheel';
+import ZingTouch from 'zingtouch';
+
+class Ipod extends React.Component{
+    constructor(){
+        super();
+    }
+    //wheel ka rotation control
+    //menu click
+    //inner circle click
+    handleRotation = () => {
+        const target = document.getElementById('scroll-wheel');
+        const zt = new ZingTouch.Region(target);
+        let angle = 0;
+        zt.bind(target, 'rotate', (e) => {
+            angle += e.detail.distanceFromLast;
+            //angle range in positive and negative 0 se 30 pe aghe and 0 se -30 pe peche jaega menu item selection
+            console.log("angle : " + angle + "\n");
+
+        })
+    }
+    render(){
+        return (
+         <div className="Ipod">
+             <Wheel
+             onHandleRotation = {this.handleRotation} />
+         </div>
+        );
+      }
+}
+
+export default Ipod;
---
+++ src/Wheel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Wheel = (props) => {
+    return(
+        <div className="container">
+            <div className="outer-circle" id="scroll-wheel" onClick={props.onHandleRotation}>
+                <img id="menu-btn" className="menu-btn" src="menu.svg" alt="menu-btn"/>
+                <img id="next-btn" className="next-btn" src="fast_forward.svg" alt="next-btn"/>
+                <img id="prev-btn" className="prev-btn" src="rewind.svg" alt="prev-btn"/>
+                <img id="play-pause-btn" className="play-pause-btn" src="play_pause.svg" alt="play-pause-btn"/>
+                <div className="inner-circle" id="centre-button"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Wheel;
